3c5bfd6fab52b29a4c2caf9eb5b1808d375977ce
Rafael Sousa <42585938+rsrbr020@users.noreply.github.com>
2018-09-14 16:35:32 -0300

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,11 @@
+<html>
+<head></head>
+<body>
+<form method="POST" action="controle/verificarlogin.php">
+<input tpe="text" name="login" placeholder="Usuário";><br/>
+<input tpe="password" name="senha" placeholder="Senha";><br/>
+<input tpe="submit" value="Entrar no siatema">
+<input type="reset" value="Limapr dados">
+</form>
+</body>
+</html>

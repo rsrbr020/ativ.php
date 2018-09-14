e23a26ade2acdc2655fd168cc5f293d919f90da9
Rafael Sousa <42585938+rsrbr020@users.noreply.github.com>
2018-09-14 16:35:06 -0300

    Delete index1.php

--- index1.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if($session['login'] = null){
-    header("Location:../index.php");
-}else{
-    echo"Bem vindo".$SESSION['login'];
-}
-?>
-<a href= "../controle/sair.php">Sair</a>
